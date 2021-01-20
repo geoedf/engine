e0ed6e6742d1179d10e8485bf9c254733729f90b
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2021-01-20 14:50:07 -0500

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='geoedfengine',
-      version='0.1',
+      version='0.2',
       description='GeoEDF Plug-and-play Workflow Engine',
       url='http://github.com/geoedf/engine',
       author='Rajesh Kalyanam',
