2712983fd7de9d43f2d2949f968fa4a13f393049
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2021-03-13 09:19:00 -0500

    GeoEDF config file can be overriden via env variable

--- geoedfengine/GeoEDFConfig.py
+++ geoedfengine/GeoEDFConfig.py
@@ -3,7 +3,8 @@
 
 """ Simple configuration class to customize a GeoEDF engine deployment
     Configuration options set in a geoedf.cfg file are parsed using ConfigParser    
-    The geoedf.cfg file is fetched from the standard path /usr/local/config
+    The geoedf.cfg file is fetched from the standard path /usr/local/config or 
+    wherever the environment variable GEOEDF_CONFIG says it is present
 """
 
 import sys
@@ -15,13 +16,13 @@ class GeoEDFConfig:
 
     def __init__(self):
         # creates a config object based on parsing config file
+        # environment variable can be used to override standard config file path
 
-        std_dir = '/usr/local/config'
-        std_dir_config = '%s/geoedf.cfg' % std_dir
+        config_filepath = str(os.getenv('GEOEDF_CONFIG','/usr/local/config/geoedf.cfg'))
 
-        if not (os.path.exists(std_dir_config) and os.path.isfile(std_dir_config)):
-            raise GeoEDFError('Error: required GeoEDF config file not found in /usr/local/config')
+        if not (os.path.exists(config_filepath) and os.path.isfile(config_filepath)):
+            raise GeoEDFError('Error: required GeoEDF config file not found; either set GEOEDF_CONFIG environment to correct path or make sure it is present in /usr/local/config')
 	    
         # parse config
         self.config = configparser.ConfigParser()
-        self.config.read(std_dir_config)
+        self.config.read(config_filepath)
